490f111c5922a940869ec40edc536a3c67c90bc2
Erdem Kalyoncu <ekalyoncu81@gmail.com>
2022-10-06 11:48:41 +0300

    Initial commit

---
+++ app/src/main/java/com/ekalyoncu/footballsquads/ui/teams/TeamsFragment.kt
@@ -0,0 +1,22 @@
+package com.ekalyoncu.footballsquads.ui.teams
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.ekalyoncu.footballsquads.R
+
+class TeamsFragment : Fragment() {
+
+    private val viewModel: TeamsViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_teams, container, false)
+    }
+
+}
---
+++ app/src/main/java/com/ekalyoncu/footballsquads/ui/teams/TeamsViewModel.kt
@@ -0,0 +1,7 @@
+package com.ekalyoncu.footballsquads.ui.teams
+
+import androidx.lifecycle.ViewModel
+
+class TeamsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
